561bea7789c109443f8ea609d65c86aef65a48c0
Elison Liu <elisonliu97@gmail.com>
2021-07-27 11:17:32 -0400

    mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,41 @@ export const ADD_USER = gql`
     }
 `;
 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: BookInput!) {
+        saveBook(book: $book) {
+            username
+            savedBooks {
+                bookId
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+            }
+        }
+    }
+`
