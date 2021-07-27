4c31bc3003c9d2bcb7797383831f9085fc78c90f
Elison Liu <elisonliu97@gmail.com>
2021-07-26 22:06:50 -0400

    typedef and resolver to add user

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,13 +1,45 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Query {
-    me: User
-}
-type Auth {
-    token: ID!
-    user: User
-}
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }    
+    
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+    }
+
 `;
 
 module.exports = typeDefs;
